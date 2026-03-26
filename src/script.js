ac059b89db188ca39654809b08106a2737183c3c
akosuaDama <damarisappiahdanso@gmail.com>
2026-03-26 13:23:21 +0000

    adding new changes

---
+++ src/script.js
@@ -0,0 +1,13 @@
+let number = 7; //assigning a value to a variable
+if(number % 2 == 0){ //using the if keyword,adding a condition to the if keyword to check if the condition is met
+    // alert(`${number} is even `) 
+    console.log(`${number} is even`) //logging the output to the console if the condition is met
+};
+
+if(number%2==0){
+    // alert(number + "is even")
+    console.log(number + "is even")
+} else {
+    console.log(`${number} is odd`)
+}
+
